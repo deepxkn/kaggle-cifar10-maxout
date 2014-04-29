af633d884394cf37ee21d4d149425fae06fe6aea
lucktroy <jbzhang86@gmail.com>
2014-04-29 21:04:43 +0800

    default update all

---
+++ make_batch_data_png2npy.py
@@ -0,0 +1,39 @@
+import png
+import image
+import numpy as np
+import cPickle
+HOME=os.path.expanduser("~")
+prefix=HOME+"/DATA/kaggle/cifar10/"
+
+def _flat2boxed(row):
+    # Note we skip every 4th element, thus eliminating the alpha channel
+    return [tuple(row[i:i+3]) for i in range(0, len(row), 3)]
+
+def png2array(path="/Users/eric/10000.png"):
+    (w, h, p, m) = png.Reader(filename = path).asRGB8()
+    img = np.asarray([_flat2boxed(r) for r in p], dtype=np.uint8)
+    data = img[...,0].reshape(1024)
+    data = np.concatenate((data, img[...,1].reshape(1024)), axis = 0)
+    data = np.concatenate((data, img[...,2].reshape(1024)), axis = 0)
+    return data.astype(np.uint8)
+    #return [_flat2boxed(r) for r in p]
+    #print z
+
+def save_data(output='batch1.pkl', data=None):
+    f = open(output, 'wb')
+    cPickle.dump(data, f)
+    f.close()
+    print 'transform data successfully'
+    return
+
+def work():
+    #num=300000
+    num=300
+    data=np.asarray([png2array(path="{}test/{}.png".format(prefix,i)) for i in range(num)])
+    print data.shape
+    np.save('data{}.npy'.format(num), data)
+
+if __name__ == '__main__':
+    work()
+
+
---
+++ make_predict.py
@@ -0,0 +1,67 @@
+﻿#-*- coding: utf-8 -*-
+#!/bin/python
+from pylearn2.utils import py_integer_types
+from pylearn2.utils import serial
+from pylearn2.config import yaml_parse
+import sys
+import numpy as np
+
+import os
+import scipy.io
+HOME=os.path.expanduser("~")
+prefix=HOME+"/DATA/kaggle/cifar10/"
+
+_, model_path = sys.argv
+
+model = serial.load(model_path)
+
+src = model.dataset_yaml_src
+batch_size = 6000
+model.set_batch_size(batch_size)
+
+def load_data(file=path+"data300000.npy".format(SNR)):
+    return np.load(file)
+
+X=load_data()
+X=X[ids]
+
+X = X.astype('float32')
+
+import theano.tensor as T
+
+Xb = model.get_input_space().make_batch_theano()
+Xb.name = 'Xb'
+
+ymf = model.fprop(Xb)
+ymf.name = 'ymf'
+
+from theano import function
+
+yp = T.argmax(ymf,axis=1)
+batch_y = function([Xb],[yp])
+
+
+def make_predict():
+    yy = []
+    #assert isinstance(X[0], (int, long))
+    assert isinstance(batch_size, py_integer_types)
+    print X.shape
+    loop = X.shape[0]/batch_size
+    if X.shape[0] % batch_size != 0 :
+        loop += 1
+    for i in xrange(loop):
+        print i,
+        x_arg = X[i*batch_size:(i+1)*batch_size,:]
+        print len(x_arg)
+        if Xb.ndim > 2:
+            x_arg = test.get_topological_view(x_arg)
+        yy.append(batch_y(x_arg) )
+    return yy
+
+y_pred = make_predict()
+
+y_pred = np.hstack(y_pred).flatten()
+
+np.save('y_pred.npy', y_pred)
+
+
