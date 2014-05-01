17ce82911a082632d6f08c73fa1438c9f40ef697
lucktroy <jbzhang86@gmail.com>
2014-05-02 07:13:35 +0800

    default update all

--- runMe.sh
+++ runMe.sh
@@ -1,4 +1,4 @@
 
 #export THEANO_FLAGS='base_compiledir=/tmp/theano/kaggle-cifar10/,device=gpu1,floatX=float32'
 
-THEANO_FLAGS="device=gpu0,floatX=float32" python make_predict.py ~/pylearn2/pylearn2/scripts/papers/maxout/predict_cifar10/cifar10_epoch328.pkl 2>time.log
+THEANO_FLAGS="device=gpu0,floatX=float32" python make_predict.py ~/pylearn2/pylearn2/scripts/papers/maxout/predict_cifar10/cifar10_epoch411.pkl 2>time_GTX780.info
