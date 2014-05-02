ce05ed70da44983a6dbad1f7d48b7054f22f0f7f
lucktroy <jbzhang86@gmail.com>
2014-05-02 08:53:23 +0800

    default update all

--- runMe.sh
+++ runMe.sh
@@ -1,6 +1,6 @@
 
 #export THEANO_FLAGS='base_compiledir=/tmp/theano/kaggle-cifar10/,device=gpu1,floatX=float32'
 
-PATH_PARAM=.
+PATH_PARAM=./maxout
 
 THEANO_FLAGS="device=gpu0,floatX=float32" python make_predict.py PATH_PARAM/predict_cifar10/cifar10_epoch442.pkl 2>time_k20m.info
